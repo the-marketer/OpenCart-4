42ca1cfcefcbd923e4c2ab933edca17be3034e1a
EAX LEX <lex@eax.ro>
2023-02-16 15:31:13 +0200

    v1

---
+++ catalog/controller/module/mktr_tracker.php
@@ -0,0 +1,26 @@
+<?php
+namespace Opencart\Catalog\Controller\Extension\Mktr\Module;
+/** @noinspection PhpUnused
+ *
+ * @copyright   © EAX LEX SRL. All rights reserved.
+ * @license     http://opensource.org/licenses/osl-3.0.php - Open Software License (OSL 3.0)
+ **/
+
+if (!defined('MKTR_ROOT')) {
+    define('MKTR_ROOT', dirname(DIR_APPLICATION) . '/');
+}
+
+if (!defined('MKTR_LIB')) {
+    if (VERSION >= "4") {
+        define('MKTR_LIB', MKTR_ROOT . '/extension/mktr/' . basename(DIR_SYSTEM). '/library/mktr/');
+    } else {
+        define('MKTR_LIB', dirname(DIR_SYSTEM) . '/' . basename(DIR_SYSTEM). '/library/mktr/');
+    }
+}
+
+/** @noinspection PhpIncludeInspection */
+require_once MKTR_LIB.'vendor/autoload.php';
+
+class MktrTracker extends \Opencart\System\Engine\Controller {
+    use \Mktr\Tracker\Catalog;
+}
